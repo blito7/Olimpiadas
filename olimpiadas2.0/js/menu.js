9bbd6f021ec3e94eb205baf501129097ee4a1f6a
Agus5 <sagu9168@gmail.com>
2025-07-04 18:35:30 -0300

    Delete olimpiadas2.0 directory

--- olimpiadas2.0/js/menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.getElementById('menu-toggle');
-  const menu = document.getElementById('main-menu');
-
-  toggle.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
-  });
-});
-
